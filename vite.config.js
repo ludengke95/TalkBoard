32620a701559a603ba98cea6f89522288895489a
ludengke95 <ludengke95@gmail.com>
2026-03-06 20:40:55 +0800

    更新项目前缀

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/TalkBoard/" : "/",
   define: {
     process: JSON.stringify({
       env: {
-        NODE_ENV: 'development'
-      }
-    })
+        NODE_ENV: "development",
+      },
+    }),
   },
   server: {
     port: 3000,
-    open: false
-  }
-})
+    open: false,
+  },
+});
